fde5c6c17e8079583d38b2d8fa5f59b3f6f0f0f9
João Pedro <joao.pedro@even3.com.br>
2023-12-19 17:35:38 -0300

    Alterações no código do qlearning

--- client.py
+++ client.py
@@ -13,19 +13,19 @@ epsilon = 0.1  # Exploração vs Exploração
 actions = ["left", "right", "jump"]
 
 # Função para escolher uma ação com base no estado atual e na política epsilon-greedy
-def choose_action(q_values, state):
+def choose_action(q_values, state, actions):
     if random.uniform(0, 1) < epsilon:
         return random.choice(actions)
     else:
         return max(actions, key=lambda a: q_values[state][a])
 
 # Função para atualizar os valores Q com base na recompensa e na próxima ação
-def update_q_values(q_values, state, action, reward, next_state):
+def update_q_values(q_values, state, action, reward, next_state, actions):
     max_next_q = max(q_values[next_state].values()) if next_state in q_values else 0
     q_values[state][action] += alpha * (reward + gamma * max_next_q - q_values[state][action])
 
 # Função principal para executar o algoritmo Q-Learning
-def q_learning(port):
+def q_learning(port, actions):
     # Inicialize os valores Q para cada par estado-ação
     q_values = defaultdict(lambda: {a: 0 for a in actions})
 
@@ -34,29 +34,38 @@ def q_learning(port):
 
     # Laço principal de treinamento
     while True:
+        try:
+            # Obtenha o estado atual e a recompensa do jogo
+            action = choose_action(q_values, 000, actions[2])
+            state, reward = get_state_reward(socket_conn, action)
 
-        # Obtenha o estado atual e a recompensa
-        print("Antes de chamar get_state_reward")
-        state, reward = get_state_reward(socket_conn, None)
-        print("Após chamar get_state_reward")
+            # Se estado ou recompensa forem inválidos, continue para a próxima iteração
+            if state is None or reward is None:
+                print("Recebido estado ou recompensa inválidos. Verifique a resposta do servidor.")
+                continue
 
-        # Escolha uma ação com base na política epsilon-greedy
-        action = choose_action(q_values, state)
-        # Execute a ação e obtenha o próximo estado e recompensa
-        next_state, next_reward = get_state_reward(socket_conn, action)
+            # Escolha uma ação com base na política epsilon-greedy
+            action = choose_action(q_values, state, actions)
 
-        # Atualize os valores Q com base na recompensa e na próxima ação
-        update_q_values(q_values, state, action, reward, next_state)
+            # Execute a ação e obtenha o próximo estado e recompensa
+            next_state, next_reward = get_state_reward(socket_conn, action)
 
-        # Salve a Q-table em um arquivo .txt
-        with open("q_table.txt", "w") as f:
-            for state, actions in q_values.items():
-                f.write(f"{state}: {actions}\n")
+            # Atualize os valores Q com base na recompensa e na próxima ação
+            update_q_values(q_values, state, action, reward, next_state, actions)
 
-        # Aguarde um momento antes da próxima iteração (pode ser ajustado)
-        time.sleep(0.1)
+            # Salve a Q-table em um arquivo .txt (opcional)
+            with open("q_table.txt", "w") as f:
+                for state, actions in sorted(q_values.items()):
+                    f.write(f"{state}: {actions}\n")
+
+            # Aguarde um momento antes da próxima iteração (pode ser ajustado)
+            time.sleep(0.1)
+
+        except Exception as e:
+            print(f"Erro durante o treinamento: {e}")
+            break
 
 if __name__ == "__main__":
     # Substitua a porta abaixo pela porta correta do seu jogo
     porta_do_jogo = 2037
-    q_learning(porta_do_jogo)
+    q_learning(porta_do_jogo, actions)
